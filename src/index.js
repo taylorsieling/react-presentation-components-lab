a930155b47840eab8925fdb53d61f9a040ba937f
taylorsieling <taylorasieling@gmail.com>
2021-05-15 15:49:15 -0500

    commit tree c8f010649f175dc485db3147881ca4e06e49af71

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(() => console.log("I am just so darn happy"))} />
   </div>,
   document.getElementById('root')
 );
